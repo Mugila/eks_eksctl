e43e512ead709c7d32269d9f57535ed47ad5c98c
Mugila <Mugilanit@live.com>
2025-12-16 08:56:16 -0500

    updated external_dns parameters

--- certmanager.sh
+++ certmanager.sh
@@ -20,13 +20,7 @@ aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${CLUSTER_REGION}
 helm repo add jetstack https://charts.jetstack.io --force-update
 #helm repo update
 # Install cert-manager with CRDs https://artifacthub.io/packages/helm/cert-manager/cert-manager 
-helm install cert-manager jetstack/cert-manager \
- --namespace "${LBC_NAMESPACE}" \  
- --set crds.enabled=true \
- --set serviceAccount.create=false \
- --set serviceAccount.name=default \  
- --set automountServiceAccountToken=true \ 
- --set startupapicheck.timeout=5m  --no-hooks 
+helm install cert-manager jetstack/cert-manager --namespace "${LBC_NAMESPACE}"  --version v1.19.2 --set crds.enabled=true --set serviceAccount.create=false --set serviceAccount.name=default --set automountServiceAccountToken=true --set startupapicheck.timeout=5m  --no-hooks 
 #--set clusterResourceNamespace=kube-system \
 
  # Tells cert-manager to look for things like DNS provider secrets in kube-system too, matching the main installation namespace. 
